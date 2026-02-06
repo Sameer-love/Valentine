05252365640ea2dc5581c0c2b7b06b6267e16e13
Sameer-love <mr.sahu.sameer@gmail.com>
2026-02-06 15:20:47 +0530

    main.jsx fixed

--- valentine-app/src/main.jsx
+++ valentine-app/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./App.css"; // we only need App.css now
+import "./app.css"; // we only need App.css now
 
 const root = createRoot(document.getElementById("root"));
 root.render(
